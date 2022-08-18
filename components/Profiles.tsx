ebac588b7546588b25f71ef614e5b07edd8e22e9
yantommy123 <tao.yan@cognizant.com>
2022-08-17 23:36:46 -0500

    1 file modified

--- components/Profiles.tsx
+++ components/Profiles.tsx
@@ -7,9 +7,23 @@ export const Profiles = () => {
 
   React.useEffect(() => {
        getProfile("tom").then((res) => {
-         
-        console.log('result ', res.data);
+         const person: Person = {
+            name: res.data.name,
+            company: res.data.company
+         };
+         const prev: Person[] = [...profiles];
+
+        // setProfiles([person, ...prev])
       });
+
+      getProfile("david").then((res) => {
+        const person: Person = {
+           name: res.data.name,
+           company: res.data.company
+        };
+        const prev = [...profiles];
+       //setProfiles([person, ...prev])
+     });
     
   });
 
